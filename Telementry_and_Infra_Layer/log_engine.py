b7f5cf2ee66562159db1e5ec282f4410c578baf3
Hevindu <95610343+Hevb890@users.noreply.github.com>
2026-09-15 12:11:39 +0530

    Cleaned Code

--- Telementry_and_Infra_Layer/log_engine.py
+++ Telementry_and_Infra_Layer/log_engine.py
@@ -10,7 +10,6 @@ class LogEngine:
     def emit_log(self, service: str) -> TelemetryEvent:
         host = self.topology.get_random_host(service)
 
-        # Handle DB Deadlock chaos mode
         if self.topolgy.db_deadlock_active and service in [
             "payment-gateway",
             "user-db",
@@ -47,7 +46,6 @@ class LogEngine:
                     },
                 )
 
-        # Handle OOM chaos mode
         if self.topology.oom_active and service == "auth-service":
             return TelemetryEvent(
                 service=service,
